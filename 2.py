f3df37c7dd00fe74cd5fdfc48ba0109763aa1039
Johan Herland <johan@herland.net>
2017-12-02 10:28:00 +0100

    Day 2 work

---
+++ 2.py
@@ -0,0 +1,28 @@
+def rows(f):
+    for line in f:
+        if line.rstrip():
+            yield [int(word) for word in line.split()]
+
+
+def rowdiff(rows):
+    for row in rows:
+        yield max(row) - min(row)
+
+
+def rowdivisor(rows):
+    for row in rows:
+        maxdiv = 1  # trivial result
+        for a in row:
+            for b in row:
+                if a % b == 0:
+                    maxdiv = max(maxdiv, a // b)
+                elif b % a == 0:
+                    maxdiv == max(maxdiv, b // a)
+        yield maxdiv
+
+
+with open("2.input") as f:
+    print(sum(rowdiff(rows(f))))
+
+with open("2.input") as f:
+    print(sum(rowdivisor(rows(f))))
