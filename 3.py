78ba779a977daef08cb33b4466ac991a732f8853
Johan Herland <johan@herland.net>
2017-12-03 16:47:25 +0100

    Make it shorter, without sacrificing (much) readability

--- 3.py
+++ 3.py
@@ -4,14 +4,10 @@ def spiral():
     r = 0
     while True:
         r += 1
-        for y in range(-r + 1, r):
-            yield r, y  # right edge
-        for x in range(r, -r, -1):
-            yield x, r  # top edge
-        for y in range(r, -r, -1):
-            yield -r, y  # left edge
-        for x in range(-r, r + 1):
-            yield x, -r  # bottom edge
+        yield from ((r, y) for y in range(-r + 1, r))  # right edge
+        yield from ((x, r) for x in range(r, -r, -1))  # top edge
+        yield from ((-r, y) for y in range(r, -r, -1))  # left edge
+        yield from ((x, -r) for x in range(-r, r + 1))  # bottom edge
 
 
 cached = [None]
@@ -26,29 +22,25 @@ def coord(n):
     return cached[n]
 
 
-def steps(coord):
+def steps(x, y):
     '''Count taxicab length from origin to coord (x, y).'''
-    x, y = coord
     return abs(x) + abs(y)
 
 
-def adjacent(coord):
+def adjacent(x, y):
     '''Generate coords adjacent to the given coord.'''
-    x, y = coord
     for dx in [-1, 0, 1]:
         for dy in [-1, 0, 1]:
             yield x + dx, y + dy
 
 
 # part 1
-print(steps(coord(361527)))
+print(steps(*coord(361527)))
 
 # part 2
-n = 1
-vals = {(0, 0): 1}
+n, vals = 1, {(0, 0): 1}
 while vals[coord(n)] < 361527:
     n += 1
-    assert coord(n) not in vals
-    vals[coord(n)] = sum(vals.get(c, 0) for c in adjacent(coord(n)))
+    vals[coord(n)] = sum(vals.get(c, 0) for c in adjacent(*coord(n)))
 
 print(vals[coord(n)])
