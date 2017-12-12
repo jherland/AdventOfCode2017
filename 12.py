7512ffc16e05f758d3c7c409bc4d5c64449efa87
Johan Herland <johan@herland.net>
2017-12-12 08:30:39 +0100

    Day 12

---
+++ 12.py
@@ -0,0 +1,28 @@
+def parse_conns(f):
+    for line in f:
+        node, arrow, conns = line.rstrip().split(' ', 2)
+        assert arrow == '<->'
+        yield int(node), set(int(c.rstrip(',')) for c in conns.split(' '))
+
+
+def find_group(node, connections):
+    seen, new = set(), {node}
+    while new:
+        seen, new = seen | new, {c for n in new - seen for c in connections[n]}
+    return seen
+
+
+# part 1
+with open('12.input') as f:
+    connections = {node: conns for node, conns in parse_conns(f)}
+group = find_group(0, connections)
+print(len(group))
+
+# part 2
+groups = 1
+connections = {k: v for k, v in connections.items() if k not in group}
+while connections:
+    group = find_group(next(iter(connections.keys())), connections)
+    connections = {k: v for k, v in connections.items() if k not in group}
+    groups += 1
+print(groups)
