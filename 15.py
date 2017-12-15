32bad5342fd29d34ea1105b81bcb6656d82f73cc
Johan Herland <johan@herland.net>
2017-12-15 09:09:31 +0100

    Day 15

---
+++ 15.py
@@ -0,0 +1,22 @@
+def gen(start, factor, modulo, picky=1):
+    while True:
+        start = (start * factor) % modulo
+        if start % picky == 0:
+            yield start
+
+
+with open('15.input') as f:
+    a_start = int(f.readline().split()[-1])
+    b_start = int(f.readline().split()[-1])
+
+# part 1
+a_gen = gen(a_start, 16807, 2147483647)
+b_gen = gen(b_start, 48271, 2147483647)
+print(sum(next(a_gen) & 0xffff == next(b_gen) & 0xffff
+          for _ in range(40000000)))
+
+# part 2
+a_gen = gen(a_start, 16807, 2147483647, picky=4)
+b_gen = gen(b_start, 48271, 2147483647, picky=8)
+print(sum(next(a_gen) & 0xffff == next(b_gen) & 0xffff
+          for _ in range(5000000)))
