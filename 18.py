498387abe899fd3cd255cd3c6efb15f15f80fd6c
Johan Herland <johan@herland.net>
2017-12-18 16:09:26 +0100

    Day 18: Unfold nested generator

--- 18.py
+++ 18.py
@@ -21,33 +21,30 @@ def thread(program, pid=0):
         except ValueError:
             return regs[s]
 
-    def do_instruction(instr, args):
-        nonlocal pc, outbox, sent
-        if instr == 'snd':
-            outbox.append(get_val(args[0]))
-            sent += 1
-        elif instr == 'set':
-            regs[args[0]] = get_val(args[1])
-        elif instr == 'add':
-            regs[args[0]] += get_val(args[1])
-        elif instr == 'mul':
-            regs[args[0]] *= get_val(args[1])
-        elif instr == 'mod':
-            regs[args[0]] %= get_val(args[1])
-        elif instr == 'rcv':
-            regs[args[0]] = yield outbox
-            outbox = []
-        elif instr == 'jgz':
-            if get_val(args[0]) > 0:
-                pc += get_val(args[1])
-                return
-        pc += 1
-
     while True:
         if pc < 0 or pc >= len(program):
             raise RuntimeError('PC out of bounds: {}'.format(pc))
         try:
-            yield from do_instruction(*program[pc])
+            instr, args = program[pc]
+            if instr == 'snd':
+                outbox.append(get_val(args[0]))
+                sent += 1
+            elif instr == 'set':
+                regs[args[0]] = get_val(args[1])
+            elif instr == 'add':
+                regs[args[0]] += get_val(args[1])
+            elif instr == 'mul':
+                regs[args[0]] *= get_val(args[1])
+            elif instr == 'mod':
+                regs[args[0]] %= get_val(args[1])
+            elif instr == 'rcv':
+                regs[args[0]] = yield outbox
+                outbox = []
+            elif instr == 'jgz':
+                if get_val(args[0]) > 0:
+                    pc += get_val(args[1])
+                    continue
+            pc += 1
         except GeneratorExit:
             print(sent)
             raise
