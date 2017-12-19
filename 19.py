ad65e2a5ce8916c96e82132ced271c1bc95e1760
Johan Herland <johan@herland.net>
2017-12-19 09:21:43 +0100

    Day 19: Wrap into Diagram class

--- 19.py
+++ 19.py
@@ -1,70 +1,71 @@
+class Diagram:
+    SYMBOLS = set('|+-ABCDEFGHIKJLMNOPQRSTUVWXYZ')
+    REVERSE = {'u': 'd', 'd': 'u', 'l': 'r', 'r': 'l'}  # up/down/left/right
+
+    @classmethod
+    def build(cls, lines):
+        '''Return Diagram instance and starting point.'''
+        lines = [line.rstrip() for line in lines]
+        height = len(lines)
+        width = max(len(line) for line in lines)
+        d = {}  # map (y, x) to symbol at (y, x). (0, 0) is top left
+        start = None  # starting point (dir_from, position, symbol)
+        for i, line in enumerate(lines):
+            for j, c in enumerate(line):
+                if c == ' ':
+                    continue
+                assert c in cls.SYMBOLS, c
+                d[(i, j)] = c
+                # start at first encountered symbol, assume we arrive from top
+                if start is None:
+                    assert i == 0 and c == '|'
+                    start = 'd', (i, j), c
+        return cls(d, height, width), start
+
+    def __init__(self, d, height, width):
+        self.d = d
+        self.height = height
+        self.width = width
+
+    def adjacent(self, y, x):
+        adjs = [
+            ('u', (y - 1, x)),
+            ('d', (y + 1, x)),
+            ('l', (y, x - 1)),
+            ('r', (y, x + 1)),
+        ]
+        for dir, (y, x) in adjs:
+            if 0 <= y < self.height and 0 <= x < self.width and (y, x) in self.d:
+                yield dir, (y, x), self.d[(y, x)]
+
+    def step(self, cur):
+        '''Walk one step from the current to the next (dir, pos, sym).'''
+        dir, pos, sym = cur
+        adjs = {dir: (dir, pos, sym) for dir, pos, sym in self.adjacent(*pos)}
+        try:
+            if sym == '+':  # change directions
+                assert len(adjs) == 2 and self.REVERSE[dir] in adjs
+                del adjs[self.REVERSE[dir]]  # eliminate where we came from
+                return adjs.popitem()[1]
+            else:  # keep going in same direction
+                return adjs[dir]
+        except KeyError:  # found the end!
+            return None
+
+    def walk(self, cur):
+        steps, found = 0, []
+        while cur:
+            steps += 1
+            sym = cur[2]
+            if sym not in set('|-+'):
+                found.append(sym)
+            cur = self.step(cur)
+        return steps, found
+
+
 with open('19.input') as f:
-    lines = [line.rstrip() for line in f]
-
-SYMBOLS = set('|+-ABCDEFGHIKJLMNOPQRSTUVWXYZ')
-DIRECTIONS = set('uplr')  # up/down/left/right
-REVERSE = {'u': 'd', 'd': 'u', 'l': 'r', 'r': 'l'}
-HEIGHT = len(lines)
-WIDTH = max(len(line) for line in lines)
-dia = {}  # map (y, x) to non-empty symbol at (y, x). (0, 0) is top left
-found = []  # letters, in the order encountered
-
-cur = None  # (dir_from, position, symbol) we're currently at
-for i, line in enumerate(lines):
-    assert 0 <= i < HEIGHT
-    for j, c in enumerate(line):
-        assert 0 <= j < WIDTH
-        if c == ' ':
-            continue
-        assert c in SYMBOLS, c
-        dia[(i, j)] = c
-        # start at first encountered symbol, assume we arrive downwards
-        if cur is None:
-            assert c == '|'
-            cur = 'd', (i, j), c
-
-
-def adjacent_symbols(pos):
-    y, x = pos
-    adjs = [
-        ('u', (y - 1, x)),
-        ('d', (y + 1, x)),
-        ('l', (y, x - 1)),
-        ('r', (y, x + 1)),
-    ]
-    for dir, (y, x) in adjs:
-        if 0 <= y < HEIGHT and 0 <= x < WIDTH and (y, x) in dia:
-            yield dir, (y, x), dia[(y, x)]
-
-
-def walk(cur):
-    '''Return next (dir, pos).'''
-    dir, pos, sym = cur
-    adjs = {dir: (dir, pos, sym) for dir, pos, sym in adjacent_symbols(pos)}
-    print(adjs)
-
-    if sym == '+':  # turn around
-        assert len(adjs) == 2  # cannot be any doubt where to turn
-        del adjs[REVERSE[dir]]  # eliminate where we came from
-        try:
-            return adjs.popitem()[1]
-        except KeyError:  # found the end!
-            return None
-    else:  # keep going in same dir
-        assert sym in set('|-ABCDEFGHIKJLMNOPQRSTUVWXYZ')
-        if sym not in set('|-'):  # found a letter!
-            found.append(sym)
-        try:
-            return adjs[dir]
-        except KeyError:  # found the end!
-            return None
-
-
-steps = 0
-while cur:
-    print(cur)
-    steps += 1
-    cur = walk(cur)
+    d, start = Diagram.build(f)
+steps, found = d.walk(start)
 
 # part 1
 print(''.join(found))
