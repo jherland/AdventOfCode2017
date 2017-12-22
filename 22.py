a2d5cb829d210300916722693522901062171c0c
Johan Herland <johan@herland.net>
2017-12-22 11:04:25 +0100

    Day 22: Rewrite/cleanup

--- 22.py
+++ 22.py
@@ -1,96 +1,79 @@
 from collections import defaultdict
 
 
-def print_grid(grid, pos, dir):
-    dim = max(abs(coord) for p in set(grid.keys()) | {pos} for coord in p)
-    dir = {'N': '^', 'E': '>', 'S': 'v', 'W': '<'}[dir]
-    for i in range(+dim, -dim - 1, -1):
-        for j in range(-dim, +dim + 1, 1):
-            print(dir if pos == (i, j) else ' ', end='')
-            print(grid[(i, j)], end='')
-            print(dir if pos == (i, j) else ' ', end='')
-        print()
-
-
+# Use text file 'coordinates': i is vertical direction, increasing downwards,
+# j is horizontal direction, increasing rightwards
 def parse_grid(f):
-    grid = set()
+    grid = defaultdict(lambda: '.')
     for i, line in enumerate(f):
         for j, c in enumerate(line.rstrip()):
             assert c in {'.', '#'}
-            if c == '#':
-                grid.add((i, j))
-
-    center = i // 2, j // 2
-    # Move center of grid to 0,0 and flip y direction
-    ret = defaultdict(lambda: '.')
-    for y, x in grid:
-        pos = center[0] - y, x - center[1]
-        ret[pos] = '#'
-    return ret
+            grid[(i, j)] = c
+    return grid, (i // 2, j // 2)  # return grid and center point
 
 
-def step1(grid, pos, dir):
-    if grid[pos] == '#':  # infected
-        dir = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}[dir]  # turn right
-        grid[pos] = '.'  # clean
-        ret = False
-    else:  # clean
-        dir = {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}[dir]  # turn left
-        grid[pos] = '#'  # infect
-        ret = True
-    y, x = pos
-    move = {'N': (y + 1, x), 'E': (y, x + 1), 'S': (y - 1, x), 'W': (y, x - 1)}
-    pos = move[dir]
-    return grid, pos, dir, ret
+def print_grid(grid, pos, dir):
+    mini = min(min(p[0] for p in grid.keys()), pos[0])
+    maxi = max(max(p[0] for p in grid.keys()), pos[0])
+    minj = min(min(p[1] for p in grid.keys()), pos[1])
+    maxj = max(max(p[1] for p in grid.keys()), pos[1])
+    arrow = {'N': '^', 'E': '>', 'S': 'v', 'W': '<'}[dir]
+    for i in range(mini, maxi + 1):
+        for j in range(minj, maxj + 1):
+            print(arrow if pos == (i, j) else ' ', end='')
+            print(grid[(i, j)], end='')
+            print(arrow if pos == (i, j) else ' ', end='')
+        print()
 
 
-def step2(grid, pos, dir):
-    # clean -> weakened -> infected -> flagged -> clean
-    next_state = {'.': 'W', 'W': '#', '#': 'F', 'F': '.'}[grid[pos]]
-    if grid[pos] == '#':  # infected
-        dir = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}[dir]  # turn right
-    elif grid[pos] == 'F':  # flagged
-        dir = {'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'}[dir]  # reverse
-    elif grid[pos] == '.':  # clean
-        dir = {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}[dir]  # turn left
-    else:  # weakened
-        assert grid[pos] == 'W'
-        # no turn
-    grid[pos] = next_state
-    y, x = pos
-    move = {'N': (y + 1, x), 'E': (y, x + 1), 'S': (y - 1, x), 'W': (y, x - 1)}
-    pos = move[dir]
-    return grid, pos, dir, next_state
+def step(grid, pos, dir, rules):
+    def move(pos, dir):
+        i, j = pos
+        return {
+            'N': (i - 1, j),
+            'E': (i, j + 1),
+            'S': (i + 1, j),
+            'W': (i, j - 1),
+        }[dir]
 
+    state, turn = rules[grid[pos]]
+    grid[pos] = state
+    dir = turn[dir]
+    pos = move(pos, dir)
+    return grid, pos, dir, state
+
+
+def run_epidemic(grid, pos, dir, num_steps, rules):
+    # print_grid(grid, pos, dir)
+    infections = 0
+    for _ in range(num_steps):
+        grid, pos, dir, state = step(grid, pos, dir, rules)
+        # print(state)
+        # print_grid(g, pos, dir)
+        if state == '#':
+            infections += 1
+    return infections
+
+
+with open('22.input') as f:
+    grid, center = parse_grid(f)
+# grid, center = parse_grid(['..#', '#..', '...'])
+
+TurnRight = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
+TurnLeft = {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}
+Reverse = {'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'}
+KeepGoing = {'N': 'N', 'E': 'E', 'S': 'S', 'W': 'W'}
+
+Part1Rules = {'.': ('#', TurnLeft), '#': ('.', TurnRight)}
+Part2Rules = {
+    '.': ('W', TurnLeft),
+    'W': ('#', KeepGoing),
+    '#': ('F', TurnRight),
+    'F': ('.', Reverse),
+}
 
 # part 1
-with open('22.input') as f:
-    grid = parse_grid(f)
-
-# grid = parse_grid(['..#', '#..', '...'])
-pos, dir = (0, 0), 'N'
-print_grid(grid, pos, dir)
-infections = 0
-for _ in range(10000):
-    grid, pos, dir, ret = step1(grid, pos, dir)
-#    print(ret)
-#    print_grid(grid, pos, dir)
-    if ret:
-        infections += 1
-print(infections)
+print(run_epidemic(grid.copy(), center, 'N', 10000, Part1Rules))
 
 # part 2
-with open('22.input') as f:
-    grid = parse_grid(f)
-
-# grid = parse_grid(['..#', '#..', '...'])
-pos, dir = (0, 0), 'N'
-print_grid(grid, pos, dir)
-infections = 0
-for _ in range(10000000):
-    grid, pos, dir, state = step2(grid, pos, dir)
-#    print(state)
-#    print_grid(grid, pos, dir)
-    if state == '#':
-        infections += 1
-print(infections)
+print(run_epidemic(grid.copy(), center, 'N', 10000000, Part2Rules))
