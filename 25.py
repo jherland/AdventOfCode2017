5d638a721937ff0672a309f2c6b155bcdb9e1ff2
Johan Herland <johan@herland.net>
2017-12-25 18:44:51 +0100

    Day 25: Read state machine from input file, instead of hardcoding

--- 25.py
+++ 25.py
@@ -1,13 +1,31 @@
-States = {  # state machine: state -> read -> (write, move, next_state)
-    'A': ((1, +1, 'B'), (0, -1, 'C')),
-    'B': ((1, -1, 'A'), (1, +1, 'D')),
-    'C': ((1, +1, 'A'), (0, -1, 'E')),
-    'D': ((1, +1, 'A'), (0, +1, 'B')),
-    'E': ((1, -1, 'F'), (1, -1, 'C')),
-    'F': ((1, +1, 'D'), (1, +1, 'A')),
-}
-tape, state, pos = {}, 'A', 0
-for _ in range(12173597):
-    tape[pos], move, state = States[state][tape.get(pos, 0)]
+with open('25.input') as f:
+    begin, steps, state, value, states = None, None, None, None, {}
+    for line in f:
+        if line.startswith('Begin in state '):
+            begin = line[15]
+        elif line.startswith('Perform a diagnostic checksum after '):
+            steps = int(line[36:].split(' ')[0])
+        elif line.startswith('In state '):
+            state = line[9]
+            states[state] = [[None, None, None], [None, None, None]]
+        elif line.strip().startswith('If the current value is '):
+            value = int(line.strip()[24])
+            assert value in {0, 1}
+        elif line.strip().startswith('- Write the value '):
+            write = int(line.strip()[18])
+            assert state and value in {0, 1} and write in {0, 1}
+            states[state][value][0] = write
+        elif line.strip().startswith('- Move one slot to the '):
+            move = {'left.': -1, 'right.': +1}[line.strip()[23:]]
+            assert state and value in {0, 1} and move
+            states[state][value][1] = move
+        elif line.strip().startswith('- Continue with state '):
+            new = line.strip()[22]
+            assert state and value in {0, 1} and new in 'ABCDEF'
+            states[state][value][2] = new
+
+tape, state, pos = {}, begin, 0
+for _ in range(steps):
+    tape[pos], move, state = states[state][tape.get(pos, 0)]
     pos += move
 print(sum(tape.values()))
